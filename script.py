fc1745b5d6ed7b4544cdc6fd6dd1e25680add86d
Elvis Leung <leung251@gmail.com>
2017-02-14 12:01:27 +0800

    Adding temp&hum operation

---
+++ script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import grovepi
+import math
+import dweepy
+import time
+
+temp_hum_sensor = 7 # for Temperature & Humidity Sensor
+while True:
+    try:
+        [temp,humidity] = grovepi.dht(sensor,0)
+        if math.isnan(temp) == False and math.isnan(humidity) == False:
+            print("temperature = %.02f C humidity =%.02f%%"%(temp, humidity))
+            dweepy.dweet_for('cba-hk-iot',{'temperature':temp,'humidity':humidity})
+        time.sleep(.01)
+
+    except IOError:
+        print ("Error")
