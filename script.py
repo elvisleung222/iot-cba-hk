092383d8bf4b15a4ca0ea3faa47b8fe89327f523
Elvis Leung <leung251@gmail.com>
2017-02-14 12:02:32 +0800

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import time
 temp_hum_sensor = 7 # for Temperature & Humidity Sensor
 while True:
     try:
-        [temp,humidity] = grovepi.dht(sensor,0)
+        [temp,humidity] = grovepi.dht(temp_hum_sensor,0)
         if math.isnan(temp) == False and math.isnan(humidity) == False:
             print("temperature = %.02f C humidity =%.02f%%"%(temp, humidity))
             dweepy.dweet_for('cba-hk-iot',{'temperature':temp,'humidity':humidity})
