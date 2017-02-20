5c7e76e5d4f788f09cf4d4f8deabd5a9d603aced
Elvis Leung <leung251@gmail.com>
2017-02-20 15:18:08 +0800

    Update operation3.py

--- operation3.py
+++ operation3.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-# from grove_rgb_lcd import * # for Grove-LCD RGB Backlight
+from grove_rgb_lcd import * # for Grove-LCD RGB Backlight
 
-# setText("Hello world\nLCD test")
-# setRGB(0,128,64)
+setText("Hello world\nLCD test")
+setRGB(0,128,64)
 
 # Slowly change the colors every 0.01 seconds.
-# for c in range(0,255):
-#     setRGB(c,255-c,0)
-#     time.sleep(0.01)
+for c in range(0,255):
+    setRGB(c,255-c,0)
+    time.sleep(0.01)
 
-# setRGB(0,255,0)
-# setText("Bye bye, this should wrap")
+setRGB(0,255,0)
+setText("Bye bye, this should wrap")
