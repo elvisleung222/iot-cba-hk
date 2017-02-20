d5ac1a8567c88b81ef545a33f5fbbaaa18d0a374
elvis <elvisleung222@hotmail.com>
2017-02-20 08:22:50 +0000

    operation3

--- operation3.py
+++ operation3.py
@@ -2,13 +2,5 @@
 
 from grove_rgb_lcd import * # for Grove-LCD RGB Backlight
 
-setText("Hello world\nLCD test")
-setRGB(0,128,64)
-
-# Slowly change the colors every 0.01 seconds.
-for c in range(0,255):
-    setRGB(c,255-c,0)
-    time.sleep(0.01)
-
-setRGB(0,255,0)
-setText("Bye bye, this should wrap")
+setText("UV Light Off")
+setRGB(0,0,0)
